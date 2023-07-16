84a7d6c7c85f31122bda7dcf2443e278e8c31969
Ali Azmy <ali.azmy99@eng-st.cu.edu.eg>
2023-07-16 16:29:18 +0300

    Function Comments of DIP module

--- HAL/DIP/DIP_Program.c
+++ HAL/DIP/DIP_Program.c
@@ -3,6 +3,14 @@
 
 
 /*Public Functions Definitions*/
+/*
+ * Function	: DIP_Init				    : Initializes the n-way SPST DIP Switch
+ * Input1 	: incpy_u8DipPort			: Port that the switch is connected to	: DIP_PORTA - DIP_PORTL
+ * Input2 	: incpy_u8NumPins	        : The number of pins of the switch (n)	: 1 - 8
+ * Input3 	: incpy_u8PullupEnable		: Enable or Disable Pull up resistors	: DIP_PULLUP, DIP_NOPULLUP
+ * Input4 	: incpy_u8OutPutInvert	    : Whether to invert the reading			: DIP_OUTPUT_INVERT, DIP_OUTPUT_NOT_INVERT
+ * Return 	:							: Error Status of function
+ */
 ErrorStatus DIP_Init(u8 incpy_u8DipPort, u8 incpy_u8NumPins, u8 incpy_u8PullupEnable, u8 incpy_u8OutPutInvert)
 {
 	ErrorStatus Loc_ErrorStatusReturn = NO_ERROR;
@@ -25,6 +33,11 @@ ErrorStatus DIP_Init(u8 incpy_u8DipPort, u8 incpy_u8NumPins, u8 incpy_u8PullupEn
 	return NO_ERROR;
 }
 
+/*
+ * Function	: DIP_Read				    : Reads the n-way SPST DIP Switch
+ * Output1	: outptr_u8Reading			: Value of read from the switch 0 - 2^(n)-1, !(0 - 2^(n)-1)
+ * Return 	:							: Error Status of function
+ */
 ErrorStatus DIP_Read(volatile u8* outptr_u8Reading)
 {
 	ErrorStatus Loc_ErrorStatusReturn = NO_ERROR;
