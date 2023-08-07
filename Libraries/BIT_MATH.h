1931634650e03c59c79afc6b31ae4a89c47c554b
AshrafElGenidy <ashrafe.elgenidy@gmail.com>
2023-08-07 21:14:53 +0300

    Missing Brackets in macros

--- Libraries/BIT_MATH.h
+++ Libraries/BIT_MATH.h
@@ -12,9 +12,9 @@
 
 /*Function-like Macros for Logical Operations on Variables or Registers*/
     /*Bits*/
-#define SET_BIT(Var,BitNo) 						(Var) = (Var)|(1<<(BitNo))
-#define CLR_BIT(Var,BitNo) 						(Var) = (Var) & ~(1<<(BitNo))
-#define TOGGLE_BIT(Var,BitNo) 					(Var) = (Var) ^ (1<<(BitNo))
+#define SET_BIT(Var,BitNo) 						(Var) = ((Var)|(1<<(BitNo)))
+#define CLR_BIT(Var,BitNo) 						(Var) = ((Var) & ~(1<<(BitNo)))
+#define TOGGLE_BIT(Var,BitNo) 					(Var) = ((Var) ^ (1<<(BitNo)))
 #define GET_BIT(Var,BitNo) 						(((Var) >> (BitNo)) & 1)
 #define MAKE_BIT(Var,BitNo,Val)					(Var) = (((Var) & (~(1<<(BitNo)))) | ((Val)<<(BitNo)))
 
@@ -27,10 +27,10 @@
 #define CLR_BYTE(Var) 							Var = 0x00
 
     /*Nibbles*/
-#define SET_NIBBLE_LOW(Var) 					Var = Var | 0x0F
-#define SET_NIBBLE_HIGH(Var) 					Var = Var | 0xF0
-#define CLEAR_NIBBLE_LOW(Var) 					Var = (Var & (0xF0))
-#define CLEAR_NIBBLE_HIGH(Var) 					Var = (Var & (0x0F))
+#define SET_NIBBLE_LOW(Var) 					(Var) = ((Var) | 0x0F)
+#define SET_NIBBLE_HIGH(Var) 					(Var) = ((Var) | 0xF0)
+#define CLEAR_NIBBLE_LOW(Var) 					(Var) = ((Var) & (0xF0))
+#define CLEAR_NIBBLE_HIGH(Var) 					(Var) = ((Var) & (0x0F))
 /*__________________________________________________________________________________________________________________________________________*/
 
 
